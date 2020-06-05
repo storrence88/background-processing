69c9dbd3eb5faa524f9ebf3e0556d34ffd8c7a82
Steven Torrence <storrence88@gmail.com>
2020-06-04 20:17:40 -0500

    Add Worker

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: bin/rails server -p ${PORT:-5000} -e $RAILS_ENV
+somethingworker: bundle exec sidekiq -c 1
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
+
   root to: 'demos#index'
   post '/create', to: 'demos#create'
   get '/show', to: 'demos#show'
